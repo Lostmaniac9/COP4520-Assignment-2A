c6f72d072d450b1e71ab2ab0f550bb7cc8250e79
Lostmaniac9 <jdog2349@gmail.com>
2022-02-23 18:34:50 -0500

    made some changes

---
+++ Party.java
@@ -0,0 +1,69 @@
+
+
+// Minotaur randomly selects guests to enter one at a time,
+// using a flag to know when to wait before sending in another guest
+class Minotaur implements Runnable
+{
+
+
+  public void run()
+  {
+    try
+    {
+      
+    }
+    catch (Exception e)
+    {
+      System.out.println("Exception has occured" + e);
+    }
+    
+    return;
+  }
+}
+
+// a guest will take a cupcake 
+class Guest implements Runnable
+{
+
+
+  public void run()
+  {
+    try
+    {
+      
+    }
+    catch (Exception e)
+    {
+      System.out.println("Exception has occured" + e);
+    }
+    
+    return;
+  }
+}
+
+class Leader implements Runnable
+{
+
+
+  public void run()
+  {
+    try
+    {
+      
+    }
+    catch (Exception e)
+    {
+      System.out.println("Exception has occured" + e);
+    }
+    
+    return;
+  }
+}
+
+class Party 
+{
+  public static void main(String args[])
+  {
+    
+  }
+}
